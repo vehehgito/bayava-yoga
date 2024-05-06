4edbf5434ccea2d51a772dd8f4cb1008ac25bd95
Neelanjan Mukherji <neelanjanmukherji@gmail.com>
2024-05-06 16:33:24 +0530

    Update Navbar component with UI improvements and responsive design

--- client/src/Components/Home/Hero.jsx
+++ client/src/Components/Home/Hero.jsx
@@ -7,7 +7,7 @@ function Hero() {
             <div className="absolute w-full h-screen bg-cover bg-center bg-[url('https://images.pexels.com/photos/1525852/pexels-photo-1525852.jpeg')]"></div>
             <div className="bg-black/30 absolute top-0 left-0 w-full h-screen"></div>
             <div className="relative h-screen w-full flex items-center justify-center">
-                <div className="absolute top-1/2 left-1/3 transform -translate-x-1/2 -translate-y-1/2 flex flex-col gap-5">
+                <div className="absolute top-1/2 left-1/3 transform -translate-x-1/2 -translate-y-1/2 flex flex-col gap-5 max-[375px]:m-12 max-[478px]:m-8 max-[575px]:m-7">
                     <div className="font-crimson uppercase">
                         <h1 className="md:text-5xl text-4xl text-white font-bold w-26 shadow-peach">Making Life a</h1>
                         <span className="md:text-7xl text-5xl text-peach font-bold shadow-white">
