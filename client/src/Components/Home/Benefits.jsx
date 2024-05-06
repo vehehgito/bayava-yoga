90aa8efe6a306e6e355e7366d89b6b11e9ceb933
Neelanjan Mukherji <neelanjanmukherji@gmail.com>
2024-05-06 21:32:07 +0530

    Update Home component with Benefits section

---
+++ client/src/Components/Home/Benefits.jsx
@@ -0,0 +1,75 @@
+import React from "react";
+
+function Benefits() {
+	return (
+		<div className="min-h-screen flex items-center justify-between gap-5 px-40 bg-peach">
+			<div className="grid grid-cols-2 gap-0">
+				<div className="p-10 w-full bg-white rounded-tl-xl border-r border-b flex flex-col gap-8 drop-shadow-md">
+					<div className="">
+						<h1 className="text-6xl">01</h1>
+						<h1 className="text-4xl">Benefit 1</h1>
+					</div>
+					<p className="text-xl">
+						Lorem ipsum dolor sit amet consectetur adipisicing elit
+					</p>
+				</div>
+				<div className="p-10 w-full bg-white rounded-tr-xl border-l border-b flex flex-col gap-8 drop-shadow-md">
+					<div className="">
+						<h1 className="text-6xl">02</h1>
+						<h1 className="text-4xl">Benefit 2</h1>
+					</div>
+					<p className="text-xl">
+						Lorem ipsum dolor sit amet consectetur adipisicing elit
+					</p>
+				</div>
+				<div className="p-10 w-full bg-white rounded-bl-xl border-r border-t flex flex-col gap-8 drop-shadow-md">
+					<div className="">
+						<h1 className="text-6xl">03</h1>
+						<h1 className="text-4xl">Benefit 3</h1>
+					</div>
+					<p className="text-xl">
+						Lorem ipsum dolor sit amet consectetur adipisicing elit
+					</p>
+				</div>
+				<div className="p-10 w-full bg-white rounded-br-xl border-l border-t flex flex-col gap-8 drop-shadow-md">
+					<div className="">
+						<h1 className="text-6xl">04</h1>
+						<h1 className="text-4xl">Benefit 4</h1>
+					</div>
+					<p className="text-xl">
+						Lorem ipsum dolor sit amet consectetur adipisicing elit
+					</p>
+				</div>
+			</div>
+			<div className="flex flex-col justify-between h-[33rem]">
+				<div className="uppercase flex flex-col h-full justify-between py-24">
+					<h4 className="text-xl font-crimson-pro text-slate">Yoga is Fun!</h4>
+					<h1 className="text-3xl">
+                        Benefits of <span className="text-7xl">Yoga</span>
+                    </h1>
+				</div>
+				<div className="flex flex-col justify-between p-5 bg-yellow rounded-xl gap-4 drop-shadow-lg">
+					<p className="text-white">
+						<div>
+							Our programs are designed to help you achieve your
+							goals.
+						</div>
+						<div>
+							We offer a variety of programs that cater to your
+							needs.
+						</div>
+					</p>
+					<div className="flex justify-end">
+						<>
+							<button className="bg-white text-yellow hover:opacity-70 px-5 py-2 rounded-md font-open-sans font-semibold min-[920px]:text-xl">
+								Learn More
+							</button>
+						</>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default Benefits;
--- client/src/Components/Home/Home.jsx
+++ client/src/Components/Home/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Hero from "./Hero";
+import Benefits from "./Benefits";
 
 function Home() {
 	return (
@@ -7,6 +8,9 @@ function Home() {
 			{/* <h1 className="text-6xl">Home</h1> */}
 			<div>
 				<Hero />
+			</div>
+			<div>
+				<Benefits />
 			</div>
 		</div>
 	);
