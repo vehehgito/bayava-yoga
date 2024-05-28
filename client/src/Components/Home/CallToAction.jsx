562d93deb6424e1c5209769c8c069687fa597fe0
Neelanjan Mukherji <neelanjanmukherji@gmail.com>
2024-05-29 02:47:59 +0530

    Frontend Bugs Fixed

--- client/src/Components/Home/CallToAction.jsx
+++ client/src/Components/Home/CallToAction.jsx
@@ -3,7 +3,7 @@ import YellowOutlineButton from "../Reusables/YellowOutlineButton";
 
 function CallToAction() {
 	return (
-			<div className="min-h-1/2 bg-smoke-brown flex items-center justify-center w-full min-[933px]:flex-row-reverse flex-col-reverse px-10 py-20 gap-10">
+			<div className="min-h-1/2 bg-smoke-brown flex items-center justify-center w-full min-[933px]:flex-row flex-col-reverse px-10 py-20 gap-10">
 				<div className="w-full flex items-center justify-center">
 					<YellowOutlineButton text="Get Started Today" />
 				</div>
--- client/src/Components/Home/HowItWorks.jsx
+++ client/src/Components/Home/HowItWorks.jsx
@@ -28,7 +28,7 @@ function HowItWorks() {
 			<div className="flex items-center justify-center w-full">
 				<h1 className="min-[933px]:text-7xl text-4xl font-bold uppercase font-crimson text-center">Our Vision</h1>
 			</div>
-			<div className="flex items-center justify-between min-[933px]:flex-row flex-col">
+			<div className="grid md:grid-cols-2 grid-cols-1 justify-center items-center w-full">
 				<div className="flex flex-col justify-between items-center gap-10">
 					<div className="flex flex-col items-center justify-center gap-5 py-4">
 						{
@@ -47,7 +47,7 @@ function HowItWorks() {
 					<img
 						src="https://plus.unsplash.com/premium_photo-1663013710516-40cf01373a2f?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
 						alt="How It Works"
-						className="w-96 h-96 object-cover rounded drop-shadow-md min-[933px]:flex hidden"
+						className="aspect-video object-cover rounded drop-shadow-md min-[933px]:flex hidden"
 					/>
 				</div>
 			</div>
