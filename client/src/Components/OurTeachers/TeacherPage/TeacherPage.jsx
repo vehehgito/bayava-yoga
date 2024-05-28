a2d2cca65a16ddbc482ee279d66ef34c2ea6f0a5
muteenk <kundangarmuteen@gmail.com>
2024-05-28 17:08:01 +0530

    Updated Teacher Display Page

--- client/src/Components/OurTeachers/TeacherPage/TeacherPage.jsx
+++ client/src/Components/OurTeachers/TeacherPage/TeacherPage.jsx
@@ -3,38 +3,48 @@ import { useParams } from "react-router-dom";
 import axios from "axios";
 
 function TeacherPage() {
-    const [teacher, setTeacher] = useState(null);
+  const [teacher, setTeacher] = useState(null);
 
-    const { teacherId } = useParams();
+  const { teacherId } = useParams();
 
-	useEffect(() => {
-		axios
-			.get(`/api/teachers/${teacherId}`)
-			.then((res) => {
-				setTeacher(res.data.rows[0]);
-			})
-			.catch((err) => {
-				console.log(err);
-			});
-	}, [teacherId]);
+  useEffect(() => {
+    axios
+      .get(`/api/teachers/${teacherId}`)
+      .then((res) => {
+        setTeacher(res.data.rows[0]);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [teacherId]);
 
-    return(
-		<div className="min-h-screen flex flex-col items-center justify-center">
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center">
+      <div>
+        {teacher ? (
+          <div className="flex justify-center items-center">
 			<div>
-				{teacher ? (
-					<div>
-						<h1>{teacher.name}</h1>
-						<p>
-							{teacher.Long_About__c}
-						</p>
-						<img src={teacher.cover_photo__c} alt={teacher.name} />
-					</div>
-				) : (
-					<p>Loading...</p>
-				)}
+				<img
+				src={teacher.cover_photo__c}
+				className="w-[20em] h-[20em] object-cover object-center rounded-full mx-[3em]"
+				alt={teacher.name}
+				/>
 			</div>
-		</div>
-    )
+            <div className="flex flex-col w-[50%]">
+				<h1>Name: {teacher.name}</h1>
+				<p>Age: {teacher.age__c}</p>
+				<p>Description: {teacher.long_About__c}</p>
+				<p>Short Description: {teacher.short_description__c}</p>
+				<p>Speciality: {teacher.speciality__c}</p>
+				<p>Type: {teacher.type__c}</p>
+			</div>
+          </div>
+        ) : (
+          <p>Loading...</p>
+        )}
+      </div>
+    </div>
+  );
 }
 
-export default TeacherPage
+export default TeacherPage;
